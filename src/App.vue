c0f9f8f36ebb606f35457617d1dbe73ec0cb08a0
Dillan Coloma <dillanrenatocolomapozo@gmail.com>
2024-07-18 19:51:20 -0500

    Taller 33 formulario estudiante 3 pg en vue

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+<EstudianteRegistro></EstudianteRegistro>
+<Estudiante></Estudiante>
+<EstudianteEliminar></EstudianteEliminar>
+</template>
+
+<script>
+import EstudianteRegistro from './components/EstudianteRegistro.vue'
+import Estudiante from './components/Estudiante.vue'
+import EstudianteEliminar from './components/EstudianteEliminar.vue'
+
+
+export default {
+  name: 'App',
+  components: {
+    //EstudianteRegistro,
+    //Estudiante,
+    EstudianteEliminar
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/EstudianteEliminar.vue
@@ -0,0 +1,81 @@
+<template>
+    <div class="container">
+      <div class="formulario">
+        <header>Borrar Estudiante</header>
+        <section>
+          <form class="form">
+            <p type="Cédula">
+              <input type="text" />
+            </p>
+            <div class="botonEliminar">
+              <button>Borrar</button>
+            </div>
+          </form>
+        </section>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  </script>
+  <style>
+   .container{
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      font-family: Verdana, Geneva, Tahoma, sans-serif;
+   }
+   body{
+      background-color: rgb(212, 235, 255);
+   }
+   p:before{
+     content: attr(type);
+     display: block;
+     margin: 5px;
+     font-size: 15px;
+     color: black;
+     
+   }
+   header{
+      color: black;
+      font-size: 35px;
+      background: rgba(140, 140, 140, 0.53);
+   }
+   input{
+      width: 200px;
+      border: none;
+      border-bottom: 2px solid #bebed2;
+  
+   }
+   input:focus{
+      outline: none;
+      border-bottom: 2px solid #78788c;
+   }
+   .form {
+      width: 300px;
+      height: 120px;
+      background-color: #e6e6e6;
+      border-radius: 10px;
+      padding: 25px 35px;
+      margin: 25px;
+      box-shadow: 0px 0px 30px 5px #000;
+  }
+  
+  button{
+      width: 100px;
+      padding: 10px 20px;
+      background-color: #a35d55;
+      color: white ;
+      border-radius: 12px;
+      cursor: pointer;
+      border: none;
+  }
+  .botonEliminar {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      font-family: Verdana, Geneva, Tahoma, sans-serif;
+      margin-top: 15px;
+      padding: 12px;
+  }
+  </style>
